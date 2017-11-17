5914033149b68d767e91e1d61a201f5ce461e562
user <user@WINDOWS-4PKKDM6>
2017-11-17 14:13:03 +0000

    Third version of Kata5

---
+++ src/pa04/main/Kata5P2.java
@@ -0,0 +1,43 @@
+package pa04.main;
+
+import java.io.IOException;
+import java.util.List;
+import pa04.model.Histogram;
+import pa04.model.Mail;
+import pa04.view.HistogramDisplay;
+import pa04.view.MailHistogramBuilder;
+import pa04.view.MailListReader;
+
+public class Kata5P2 {
+    
+    public String filename;
+    public List<Mail> mailList;
+    Histogram<String> histogram;
+    HistogramDisplay histoDisplay;
+     
+     public static void main(String[] args) throws IOException, Exception {
+         Kata5P2 kata5 = new Kata5P2();
+         kata5.execute();
+    }
+     
+    public void execute() throws IOException {
+            input();
+            process();
+            output();
+        }
+
+    public void input() throws IOException {
+        filename = "C:\\Users\\user\\Desktop\\emails.txt";    
+        mailList = MailListReader.read(filename);
+    }
+
+    public void process() throws IOException {
+        histogram = MailHistogramBuilder.build(mailList);
+        
+    }
+
+    public void output() {
+        histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+    }
+}
